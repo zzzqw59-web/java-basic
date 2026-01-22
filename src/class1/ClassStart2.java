ada41b7e188ce97f1bc804dd9955d2fd37c2d3db
sungmin <95615086+Clariatinet@users.noreply.github.com>
2026-01-22 16:57:51 +0900

    클래스가 필요한 이유

---
+++ src/class1/ClassStart2.java
@@ -0,0 +1,13 @@
+package class1;
+
+public class ClassStart2 {
+    public static void main(String[] args) {
+        String[] studentNames = {"학생1", "학생2", "학생3"};
+        int[] studentAges = {15, 20, 25};
+        int[] studentGrades = {90, 80, 100};
+
+        for (int i = 0; i < studentNames.length; i++) {
+            System.out.println("이름: " + studentNames[i] + " 나이: " + studentAges[i] + " 성적: " + studentGrades[i]);
+        }
+    }
+}
