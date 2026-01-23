c6c82d51ff1dec865b1690636a11dab0df1a36a1
sungmin <95615086+Clariatinet@users.noreply.github.com>
2026-01-23 18:19:32 +0900

    클래스 도입

---
+++ src/class1/ClassStart3.java
@@ -0,0 +1,25 @@
+package class1;
+
+public class ClassStart3 {
+    public static void main(String[] args) {
+        Student student1;
+        student1 = new Student(); // x001
+        student1.name = "학생1";
+        student1.age = 15;
+        student1.grade = 90;
+
+        Student student2 = new Student(); // x002
+        student2.name = "학생2";
+        student2.age = 20;
+        student2.grade = 80;
+
+        Student student3 = new Student(); // x003
+        student3.name = "학생3";
+        student3.age = 25;
+        student3.grade = 100;
+
+        System.out.println("이름: " + student1.name + " 나이: " + student1.age + " 등급: " + student1.grade);
+        System.out.println("이름: " + student2.name + " 나이: " + student2.age + " 등급: " + student2.grade);
+        System.out.println("이름: " + student3.name + " 나이: " + student3.age + " 등급: " + student3.grade);
+    }
+}
