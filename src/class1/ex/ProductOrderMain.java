fcd1a947047c9a0c1fd59945961b3f8a8b483555
sungmin <95615086+Clariatinet@users.noreply.github.com>
2026-02-02 22:34:44 +0900

    test

--- src/class1/ex/ProductOrderMain.java
+++ src/class1/ex/ProductOrderMain.java
@@ -21,8 +21,6 @@ public class ProductOrderMain {
         order3.price = 1500;
         order3.quantity = 2;
         orders[2] = order3;
-        orders[2] = order3;
-        orders[2] = order3;
 
         int totalAmount = 0;
         for (ProductOrder order : orders) {
