90f9c1495f05ec6a5f8429bf5ca4c0758b037e45
Clariatinet <zzzqw59@gmail.com>
2026-02-04 13:38:52 +0900

    null

---
+++ src/ref/NullMain3.java
@@ -0,0 +1,12 @@
+package ref;
+
+public class NullMain3 {
+    public static void main(String[] args) {
+        BigData bigData = new BigData(); // x001
+        System.out.println("bigData.count = " + bigData.count);
+        System.out.println("bigData.data = " + bigData.data);
+
+        // NullPointerException
+        System.out.println("bigData.data.value: " + bigData.data.value);
+    }
+}
---
+++ src/ref/NullMain4.java
@@ -0,0 +1,13 @@
+package ref;
+
+public class NullMain4 {
+    public static void main(String[] args) {
+        BigData bigData = new BigData(); // x001
+        bigData.data = new Data();
+        System.out.println("bigData.count = " + bigData.count);
+        System.out.println("bigData.data = " + bigData.data);
+
+        // NullPointerException
+        System.out.println("bigData.data.value: " + bigData.data.value);
+    }
+}
