e5101bb68b9ea8eb78dacb6030e634cb3a1d70e6
Clariatinet <zzzqw59@gmail.com>
2026-03-23 21:31:24 +0900

    복기

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -4,6 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         int score = 80;
         boolean value = score >= 80 && score <= 100;
-//        System.out.println(value);
+        System.out.println(value);
     }
 }
