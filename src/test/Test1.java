61d6224c1e8fbb5834f3ef5f5d734d086cb8e8eb
Clariatinet <zzzqw59@gmail.com>
2026-03-23 21:46:14 +0900

    복기중

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -2,8 +2,26 @@ package test;
 
 public class Test1 {
     public static void main(String[] args) {
-        int score = 80;
-        boolean value = score >= 80 && score <= 100;
-//        System.out.println(value);
+        int age = 14;
+
+        if (age <= 7) {
+            System.out.println("미취");
+        }
+
+        if (age >= 8 && age <= 13) {
+            System.out.println("초등학생");
+        }
+
+        if (age >= 14 && age <= 16) {
+            System.out.println("중학생");
+        }
+
+        if (age >= 16 && age <= 19) {
+            System.out.println("고등학");
+        }
+
+        if (age >= 20) {
+            System.out.println("성인");
+        }
     }
 }
