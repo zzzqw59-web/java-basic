eee76e143786a35fdb0d97a1aff4454f1f9852ae
Clariatinet <zzzqw59@gmail.com>
2026-03-23 23:23:32 +0900

    복기중 - 조건문

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -2,20 +2,10 @@ package test;
 
 public class Test1 {
     public static void main(String[] args) {
-        int price = 10000;
-        int age = 10;
-        int discount = 0;
+        int x = 1;
 
-        if (price >= 10000) {
-            discount = discount + 1000;
-            System.out.println("10000원 이상일 때, 1000원 할인");
-        }
 
-        if (age <= 10) {
-            discount = discount + 1000;
-            System.out.println("10살 이하의 경우, 1000원 할인");
-        }
-
-        System.out.println("총 할인 금액" + discount);
+        String result = (x % 2 == 0) ? "짝수" : "홀수";
+        System.out.println(result);
     }
 }
