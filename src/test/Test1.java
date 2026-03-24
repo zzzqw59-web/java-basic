18ba13318a0cc549d59a4c74785b7733ba8f32e4
Clariatinet <zzzqw59@gmail.com>
2026-03-24 23:38:47 +0900

    복기 - 배열

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -4,10 +4,7 @@ import java.util.Scanner;
 
 public class Test1 {
     public static void main(String[] args) {
-        int[] students = {90, 80, 70, 60, 50};
+        int[] arr = {1, 2, 3, 4, 5,6};
 
-        for (int i = 0; i < students.length; i++) {
-            System.out.println("학생" + (i + 1) + " 점수: " + students[i]);
-        }
     }
 }
