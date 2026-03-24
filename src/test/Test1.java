8d488d0be59832d5f827caaaa54b6c211ab97fb8
Clariatinet <zzzqw59@gmail.com>
2026-03-25 00:11:08 +0900

    복기 - 메서드

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 
 public class Test1 {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5,6};
+        int num1 = 5;
+        System.out.println("changeNumber 호출 전, num1: " + num1);
+        num1 = changeNumber(num1);
+        System.out.println("changeNumber 호출 후, num1: " + num1);
+    }
 
+    public static int changeNumber(int num2) {
+        num2 = num2 * 2;
+        return num2;
     }
 }
