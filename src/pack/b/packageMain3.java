5a11ffc1debea919d69ea5fce742a44a630564b3
Clariatinet <zzzqw59@gmail.com>
2026-03-31 12:43:23 +0900

    패키지

---
+++ src/pack/b/packageMain3.java
@@ -0,0 +1,10 @@
+package pack.b;
+
+import pack.a.User;
+
+public class packageMain3 {
+    public static void main(String[] args) {
+        User userA = new User();
+        pack.b.User userB = new pack.b.User();
+    }
+}
