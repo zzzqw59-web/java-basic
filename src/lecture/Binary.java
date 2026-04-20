7d6d41d6d13c3f4930c4c52539c00d6d12fa3df8
Clariatinet <zzzqw59@gmail.com>
2026-04-20 12:10:48 +0900

    자료형1

---
+++ src/lecture/Binary.java
@@ -0,0 +1,15 @@
+package lecture;
+
+public class Binary {
+    public static void main(String[] args) {
+        int num = 10;
+        int bNum = 0B1010; //10
+        int oNum = 012; //10
+        int hNum = 0XA;
+
+        System.out.println(num);
+        System.out.println(bNum);
+        System.out.println(oNum);
+        System.out.println(hNum);
+    }
+}
---
+++ src/lecture/CharacterEx.java
@@ -0,0 +1,17 @@
+package lecture;
+
+public class CharacterEx {
+    public static void main(String[] args) {
+        char ch = 'A';
+        System.out.println(ch);
+        System.out.println((int) ch);
+
+        ch = 66;
+        System.out.println(ch);
+        System.out.println((int) ch);
+
+        int ch2 = 67;
+        System.out.println(ch2);
+        System.out.println((char) ch2);
+    }
+}
