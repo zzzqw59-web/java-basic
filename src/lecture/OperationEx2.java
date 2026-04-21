e701c09dc64c9022acb37722f731ff2f9ae1bcb4
Clariatinet <zzzqw59@gmail.com>
2026-04-21 10:45:59 +0900

    연산자1

---
+++ src/lecture/OperationEx2.java
@@ -0,0 +1,16 @@
+package lecture;
+
+public class OperationEx2 {
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.println(num++);
+        System.out.println(num);
+
+        System.out.println(3 < 5);
+
+        int num1 = 10;
+        int num2 = 20;
+        boolean check = num1 > num2;
+        System.out.println(check);
+    }
+}
---
+++ src/lecture/OperationEx3.java
@@ -0,0 +1,13 @@
+package lecture;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int i = 2;
+
+        boolean value = ((num1 = num1 + 10) < 10) && ((i = i + 2) > 10);
+        System.out.println(value);
+        System.out.println(num1);
+        System.out.println(i);
+    }
+}
