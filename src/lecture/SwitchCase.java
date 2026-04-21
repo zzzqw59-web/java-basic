92e9a77dd3759f80a16411685c96d4006bfef51e
Clariatinet <zzzqw59@gmail.com>
2026-04-21 11:19:37 +0900

    조건문

---
+++ src/lecture/SwitchCase.java
@@ -0,0 +1,19 @@
+package lecture;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        int rank = 5;
+        char medalColor;
+
+        switch (rank) {
+            case 1: medalColor = 'G';
+            break;
+            case 2: medalColor = 'S';
+            break;
+            case 3: medalColor = 'B';
+            break;
+            default: medalColor = 'A';
+        }
+        System.out.println(rank + "등의 메달의 색깔은 " + medalColor + "입니다.");
+    }
+}
